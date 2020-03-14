907a1eb601e797dba4bb1b8510add389f81a197f
Rohan <31851961+rohanthakran@users.noreply.github.com>
2020-03-14 12:37:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -166,6 +166,8 @@ var UIController = (function(){
         document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
         document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
         document.querySelector(DOMstrings.expenseLabel).textContent =obj.totalExp;
+        document.querySelector(DOMstrings.percentageLabel).textContent =obj.percentageLabel;
+
         
         if (obj.percentage > 0) {
             document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
@@ -237,6 +239,12 @@ var controller = (function(budgetCntrl, UiCtrl){
       init : function() {
 
         console.log('Application has started');
+        UiCtrl.displayBudget({
+          budget: 0,
+          totalInc: 0,
+          totalExp: 0,
+          percentage: -1
+        });
         setEventListner();
         
       }
